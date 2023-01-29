e21e5831890e1a58d004ba75b7718440348bb5c1
kamiliano1 <kamiliano1991@gmail.com>
2023-01-29 20:52:09 +0100

    API Geo and Leaflet
    Setted both API to work together and when we wrote IP adress it will show the location of it

--- ip-address-tracker/src/App.jsx
+++ ip-address-tracker/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react'
-// import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-// import 
-// import reactLogo from './assets/react.svg'
-// import './App.css'
-import L from "leaflet"
+import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet'
+// import L from "leaflet"
+
 function App() {
+  // const mapRef = useRef();
+
+  const [map, setMap] = useState(()=>null)
+  const [position, setPosition] = useState({lat:51.505, long:-0.09})
+  const [typed, setTyped] = useState({lat:"", long:""})
+  // const [position, setPosition] = useState(()=>[51.505, -0.09])
   // const [ipAdress, setIpAdress] = useState(null)
   const [ipAdress, setIpAdress] = useState( ()=> {
-
     return {
       ip: "8.8.8.8",
       isp: "Google LLC",
@@ -21,72 +24,175 @@ function App() {
   }
   )
 
-  // console.log(L.version)
 
+
+
+function onClickk(e) {
+  e.preventDefault()
+  // setPosition({lat:typed.lat, long:typed.long})
+  fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_9EuNfN5MKmLWFLAw25PFyR3RiFCNA&ipAddress=${typed.lat}`)
+  // fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_9EuNfN5MKmLWFLAw25PFyR3RiFCNA&ipAddress=8.8.8.8")
+  .then(res=>res.json())
+  .then(data=>{
+    console.log(data)
+    setIpAdress({
+      ip:data.ip,
+      isp:data.isp,
+      location:data.location.city,
+      region:data.location.region,
+      postalCode:data.location.postalCode,
+      timezone:data.location.timezone,
+      latitude:data.location.lat,
+      longitude:data.location.lng,
+  })
   // console.log(ipAdress)
-  // useEffect(()=>{
-  //   fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_9EuNfN5MKmLWFLAw25PFyR3RiFCNA&ipAddress=8.8.8.8")
-  //     .then(res=>res.json())
-  //     .then(data=>setIpAdress({
-  //         ip:data.ip,
-  //         isp:data.isp,
-  //         location:data.location.city,
-  //         region:data.location.region,
-  //         postalCode:data.location.postalCode,
-  //         timezone:data.location.timezone,
-  //         latitude:data.location.lat,
-  //         longitude:data.location.lng,
+  // setPosition({lat:ipAdress.latitude, long:ipAdress.longitude})
+  // console.log(position)
+}
+    )
+
+  
+  // setTimeout(()=>{
+
+  //   map.flyTo(position, 14,{
+  //     duration: 2
+  //   })
+
+  // },1000)
+  // console.log(map)
+
+
+
+  // map.flyTo(position, 14,{
+  //       duration: 2
   //     })
-  //       )
-  // },[])
+}
+
+// function FlyToTarget() {
+//   const map = useMap()
+//   position ? map.flyTo([position.lat, position.long], 14,{
+//     duration: 3
+//   }
+//   )
+//   : null
+// }
+
+function FlyToTarget() {
+  const map = useMap()
+  ipAdress ? map.flyTo([ipAdress.latitude, ipAdress.longitude], 14,{
+    duration: 3
+  }
+  )
+  : null
+}
+
+function handleChange(e) {
+  setTyped(prevFormData => {
+      return {
+          ...prevFormData,
+          [e.target.name]: e.target.value
+      }
+  })
+}
 
 
-  // setIpAdress()
+// useEffect(()=>{
 
-  // console.log(ipAdress)
+// },[position])
 
-//   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+// function LocationMarker(props) {
+
+//   const map = useMapEvents({
+//     click() {
+//       map.flyTo([props.lat, props.lon], 14,{
+//         duration: 2
+//       })
+
+//     },
+
+//   })
+
+//   return [props.lat, props.lon] === null ? null : (
+//     <Marker position={[props.lat, props.lon]}>
+//       <Popup>You are here</Popup>
+//     </Marker>
+//   )
+// }
 // function MyComponent() {
 //   const map = useMap()
 //   console.log('map center:', map.getCenter())
 //   return null
 // }
 
-// latitude: 37.38605,
-// location: "Mountain View",
-// longitude: -122.08385,
+// setTimeout(()=>{
+//   setPosition([Math.floor(Math.random()*30), Math.floor(Math.random()*30)])
 
-useEffect(()=>{
+// },5000)
 
-  let map = L.map('map').setView([ipAdress.latitude, ipAdress.longitude], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+const handleSubmit = (event) => {
+  event.preventDefault();
+};
 
-  let marker = L.marker([ipAdress.latitude, ipAdress.longitude]).addTo(map);
-  marker.bindPopup("<b>You are</b><br>HERE").closePopup();
-})
 
-// https://leafletjs.com/examples/quick-start/
-// https://leafletjs.com/examples.html
+// function FlyToButton() {
+//   const onClick = () => map.flyTo([position.lat, position.long], 13);
+//   console.log(Math.floor(Math.random()*30))
+    
+//   return <button onClick={onClick}>Add marker on click</button>;
+// }
   return (
-                                 
-  <div id="map">
+  <div className="">
+      <MapContainer center={[ipAdress.latitude, ipAdress.longitude] || { lat: 50, lng: 30 }}  zoom={13} scrollWheelZoom={true} ref={setMap}>
+      {/* <MyComponent /> */}
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[ipAdress.latitude, ipAdress.longitude]}>
+      <Popup>
+        
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+    {/* <Marker position={[11.505, -1.09]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker> */}
+    {/* <LocationMarker 
+    lat={position[0]}
+    lon={position[1]}/> */}
+    
+  <FlyToTarget />
+  </MapContainer>
+  {/* <FlyToButton /> */}
+  <form onSubmit={handleSubmit}>
+  <input
+      type="text"
+      placeholder="First Name"
+      onChange={handleChange}
+      name="lat"
+      value={typed.lat}
+  />
+    <input
+      type="text"
+      placeholder="First Name"
+      onChange={handleChange}
+      name="long"
+      value={typed.long}
+  />
+  <button  onClick={onClickk} >Click</button>
+  </form>
+                    {/* <button onClick={handleOnFlyTo} >Click2</button> */}
+  {/* <div id="map">
 
-  </div>
+
+</div> */}
+                  </div>               
+
 
   )
 
-}
-
-
-
-// lokacja timeZone ISP
-
-
+  }
 export default App
 
